2f0e77db2027797e3e2f549e50ee171792522983
Tushar <tushar@aueb.gr>
2017-06-02 11:43:16 +0300

    refactoring to the repo structure

--- smellCatalog/HtmlGenerator.py
+++ smellCatalog/HtmlGenerator.py
@@ -1,50 +1,6 @@
 import os
-
-TOP_TEXT = "<html><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" +\
-    "<meta name=\"description\" content=\"An attempt to present a taxonomy of software smells by cataloging, classifying, and " +\
-                    "inter-relating smell definitions present in existing literature.\">" +\
-    "<meta name=\"keywords\" content=\"Code smells, architecture smells, database smells, configuration smells, test smells, catalog, taxonomy, classification\">" +\
-"<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">" +\
-"<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Poppins\">"\
-"<style>body,h1,h2,h3,h4,h5 {font-family: \"Poppins\", sans-serif}" +\
-"body {font-size: 16px;}" +\
-    ".w3-half img {margin-bottom: -6px;margin-top: 16px;opacity: 0.8;cursor: pointer}" +\
-    ".w3-half img:hover {opacity: 1}"+\
-"</style>"+\
-"<head><title>A Taxonomy of Software Smells</title></head>"
-
-BODY_TOP_PART = "<body>" +\
-    "<nav class=\"w3-sidebar w3-gray w3-collapse w3-top w3-large w3-padding\" style=\"z-index:3;width:300px;font-weight:bold;\" id=\"mySidebar\"><br>" +\
-        "<div class=\"w3-container\">" +\
-            "<h3 class=\"w3-padding-64\"><b>A Taxonomy of Software Smells</b></h3></div>" +\
-        "<div class=\"w3-bar-block\">"
-
-BODY_INDEX = "<a href=\"index.html\" class=\"w3-bar-item w3-button w3-hover-white\">Home</a>"
-
-BODY_LOW_PART = "</div>" +\
-        "<hr>" +\
-        "<div class=\"w3-bar-block\"><a href=\"http://www.tusharma.in\" class=\"w3-bar-item w3-button w3-hover-white\">Tushar's Blog</a></div>" +\
-        "<hr>" +\
-        "<div class=\"w3-bar-block\"><a href=\"http://bit.ly/DesignSmells\"><img src=\"book_cover.png\" style=\"width:220px;\"></a></div>" +\
-        "<hr>" +\
-        "<div>Detect smells using Designite<a href=\"http://www.designite-tools.com\"><img src=\"designite.png\" style=\"width:220px;\"></a></div>" +\
-    "<hr></nav>"
-
-BODY_MAIN_TOP = "<div class=\"w3-main\" style=\"margin-left:340px;margin-right:40px\">"
-
-ATTRIBUTION_TEXT = "<div class=\"w3-light-grey w3-container w3-padding-32\" style=\"margin-top:75px;padding-right:58px\">"+\
-                   "<p class=\"w3-right\">All rights reserved (c) <a href=\"http://www.tusharma.in\">Tushar Sharma</a> 2017.</p></div>"
-HTML_END_TEXT = "</body></html>"
-
-TRACKING_TEXT = "<script>" +\
-  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +\
-  "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +\
-  "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +\
-  "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');" +\
-  "ga('create', 'UA-56403135-3', 'auto');" +\
-  "ga('send', 'pageview');"+\
-"</script>"
-
+import datetime
+import FixedText
 
 class HtmlGenerator(object):
     def __init__(self, output_path, smell_list, category_list):
@@ -71,16 +27,16 @@ class HtmlGenerator(object):
 
     def generate_category(self, category):
         path = os.path.join(self.out_path, category.id + ".html")
-        self.writeFile(path, TOP_TEXT)
-        self.appendFile(path, BODY_TOP_PART)
-        self.appendFile(path, BODY_INDEX)
+        self.writeFile(path, FixedText.TOP_TEXT)
+        self.appendFile(path, FixedText.BODY_TOP_PART)
+        self.appendFile(path, FixedText.BODY_INDEX)
         cat_list = sorted(self.category_list, key=lambda cat: cat.name)
         for cat in cat_list:
             if (cat.parent_obj == None):
                 text = "<a href=\"" + cat.id + ".html\" class=\"w3-bar-item w3-button w3-hover-white\">" + cat.name + "</a>"
                 self.appendFile(path, text)
-        self.appendFile(path, BODY_LOW_PART)
-        self.appendFile(path, BODY_MAIN_TOP)
+        self.appendFile(path, FixedText.BODY_LOW_PART)
+        self.appendFile(path, FixedText.BODY_MAIN_TOP)
         child_categories = self.get_child_categories(category)
         if (len(child_categories)>0):
             self.appendFile(path, "<h2>(Sub-)Categories</h2>")
@@ -100,31 +56,24 @@ class HtmlGenerator(object):
         self.appendFile(path, "<hr>")
         self.appendFile(path, "<a href=\"index.html\"><h3>Home</h3></a>")
         self.appendFile(path, "</div>")
-        self.appendFile(path, ATTRIBUTION_TEXT)
-        self.appendFile(path, TRACKING_TEXT)
-        self.appendFile(path, HTML_END_TEXT)
+        self.appendFile(path, FixedText.ATTRIBUTION_TEXT)
+        self.appendFile(path, FixedText.TRACKING_TEXT)
+        self.appendFile(path, FixedText.SOCIAL_TEXT)
+        self.appendFile(path, FixedText.HTML_END_TEXT)
 
     def generate_index(self):
         path = os.path.join(self.out_path, "index.html")
-        self.writeFile(path, TOP_TEXT)
-        self.appendFile(path, BODY_TOP_PART)
-        self.appendFile(path, BODY_INDEX)
+        self.writeFile(path, FixedText.TOP_TEXT)
+        self.appendFile(path, FixedText.BODY_TOP_PART)
+        self.appendFile(path, FixedText.BODY_INDEX)
         cat_list = sorted(self.category_list, key=lambda cat: cat.name)
         for cat in cat_list:
             if (cat.parent_obj == None):
                 text = "<a href=\"" + cat.id + ".html\" class=\"w3-bar-item w3-button w3-hover-white\">" + cat.name + "</a>"
                 self.appendFile(path, text)
-        self.appendFile(path, BODY_LOW_PART)
-        self.appendFile(path, BODY_MAIN_TOP)
-        main_text = "<h1>A Taxonomy of Software Smells</h1>" +\
-            "<img src=\"smells.png\" style=\"width:700px;\">" +\
-        "<p>Kent Beck coined the term \"code smell\" in the popular <a href=\"http://amzn.to/2rblDm3\" target=\"_blank\"> " +\
-                    "Refactoring book by Martin Fowler</a> and defined it informally as certain structures in the code " +\
-                    "that suggest (sometimes they scream for) the possibility of refactoring. " +\
-                    "Since then, various smells have been reported that impair software quality in one or more ways. " +\
-                    "I attempt to prepare and present a taxonomy of software smells by cataloging, classifying, and " +\
-                    "inter-relating smell definitions present in existing literature with their references.</p>"
-        self.appendFile(path, main_text)
+        self.appendFile(path, FixedText.BODY_LOW_PART)
+        self.appendFile(path, FixedText.BODY_MAIN_TOP)
+        self.appendFile(path, FixedText.INTRO_TEXT)
         self.appendFile(path, "<ul>")
         for cat in cat_list:
             if (cat.parent_obj == None):
@@ -144,42 +93,39 @@ class HtmlGenerator(object):
         self.appendFile(path, "</ul>")
         total_text = "<p><b>Total documented smells: " + str(len(self.smell_list)) + "</b></p>"
         self.appendFile(path, total_text)
-        how_text = "<p>The taxonomy is generated by a python program that I wrote to generate html pages. " +\
-                   "I provide smell description as well as corresponding references in a specific format and " +\
-                   "supply them to the python program. The program generates required html pages with links " +\
-                   "that inter-relate the generated html pages. I used basic CSS to look the collection of the html files a little better.</p>"
-        self.appendFile(path, how_text)
-        additional_text = "<p>This taxonomy is evolving. I plan to add many more smells (for instance, lexicon smells, "+\
-                          "performance smells, and energy smells). If you would like to point me to a (missing) smell "+\
-                          "or reference, or would like to suggest something, feel free to email to me at tusharsharma@ieee.org.</p>"
-        self.appendFile(path, additional_text)
-        ack_text = "<p><h5>Acknowledgement:</h5> I would like to thank Prof. Diomidis Spinellis for suggesting me many improvements.</p>"
-        self.appendFile(path, ack_text)
+        self.appendFile(path, FixedText.HOW_TEXT)
+        self.appendFile(path, FixedText.ADDITIONAL_TEXT)
+        self.appendFile(path, FixedText.ACKOWLEDGEMENTS)
+        today = datetime.datetime.today()
+        self.appendFile(path, "<hr><p>Last updated: " + str(today.strftime("%B %d, %Y")) + "</p>")
         self.appendFile(path, "</div>")
-        self.appendFile(path, ATTRIBUTION_TEXT)
-        self.appendFile(path, TRACKING_TEXT)
-        self.appendFile(path, HTML_END_TEXT)
+        self.appendFile(path, FixedText.ATTRIBUTION_TEXT)
+        self.appendFile(path, FixedText.TRACKING_TEXT)
+        self.appendFile(path, FixedText.SOCIAL_TEXT)
+        self.appendFile(path, FixedText.HTML_END_TEXT)
 
     def writeFile(self, fileName, text):
-        with open(fileName, "w", errors='ignore') as f:
+        file = os.path.abspath(fileName)
+        with open(file, "w", errors='ignore') as f:
             f.write(text)
 
     def appendFile(self, fileName, text):
-        with open(fileName, "a", errors='ignore') as f:
+        file = os.path.abspath(fileName)
+        with open(file, "a", errors='ignore') as f:
             f.write(text)
 
     def generate_html(self, smell):
         path = os.path.join(self.out_path, smell.id + ".html")
-        self.writeFile(path, TOP_TEXT)
-        self.appendFile(path, BODY_TOP_PART)
-        self.appendFile(path, BODY_INDEX)
+        self.writeFile(path, FixedText.TOP_TEXT)
+        self.appendFile(path, FixedText.BODY_TOP_PART)
+        self.appendFile(path, FixedText.BODY_INDEX)
         cat_list = sorted(self.category_list, key=lambda cat: cat.name)
         for cat in cat_list:
             if (cat.parent_obj == None):
                 text = "<a href=\"" + cat.id + ".html\" class=\"w3-bar-item w3-button w3-hover-white\">" + cat.name + "</a>"
                 self.appendFile(path, text)
-        self.appendFile(path, BODY_LOW_PART)
-        self.appendFile(path, BODY_MAIN_TOP)
+        self.appendFile(path, FixedText.BODY_LOW_PART)
+        self.appendFile(path, FixedText.BODY_MAIN_TOP)
         smell_name = "<h3>" + smell.name + "</h3>"
         self.appendFile(path, smell_name)
         smell_description = "<p>" + smell.description + "</p>"
@@ -200,9 +146,10 @@ class HtmlGenerator(object):
         self.appendFile(path, "<a href=\"" + smell.category_obj.id + ".html\"><h4>" + smell.category_obj.name + "</h4></a>")
         self.appendFile(path, "<a href=\"index.html\"><h3>Home</h3></a>")
         self.appendFile(path, "</div>")
-        self.appendFile(path, ATTRIBUTION_TEXT)
-        self.appendFile(path, TRACKING_TEXT)
-        self.appendFile(path, HTML_END_TEXT)
+        self.appendFile(path, FixedText.ATTRIBUTION_TEXT)
+        self.appendFile(path, FixedText.TRACKING_TEXT)
+        self.appendFile(path, FixedText.SOCIAL_TEXT)
+        self.appendFile(path, FixedText.HTML_END_TEXT)
 
 
     def get_smell_count_in_category(self, cat):
--- smellCatalog/Main.py
+++ smellCatalog/Main.py
@@ -2,22 +2,14 @@ from InputProcessor import InputProcessor
 from HtmlGenerator import HtmlGenerator
 import Validator
 
-INPUT_FULE_PATH = "/Users/Tushar/Documents/Research/smells/smells.txt"
-OUTPUT_PATH = "/Users/Tushar/Documents/Research/smells/smellCatalogHtml/"
-SMELL_CATEGORY_FILE_PATH = "/Users/Tushar/Documents/Research/smells/smell-category.txt"
-REF_FILE_PATH = "/Users/Tushar/Documents/Research/smells/references.txt"
-
-def print_smell_list(smell_list):
-    for smell in smell_list:
-        print("id:" + smell.id)
-        print("name:" + smell.name)
-        print("desription:" + smell.description)
-        print(("aka:" + smell.aka))
-        print("category:" + smell.category)
+INPUT_FULE_PATH = "../data/smells.txt"
+OUTPUT_PATH = "../smellCatalogHtml/"
+SMELL_CATEGORY_FILE_PATH = "../data/smell-category.txt"
+REF_FILE_PATH = "../data/references.txt"
 
 input_processor = InputProcessor(INPUT_FULE_PATH)
 smell_list = input_processor.process()
-#print_smell_list(smell_list)
+
 Validator.validate_id(smell_list)
 input_processor.populate_aka_obj(smell_list)
 
